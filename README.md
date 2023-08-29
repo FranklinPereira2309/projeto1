58fa041dfbb434baf703b22ec338be4a59ef4b0e
Franklin Menezes <fmpereira2309@hotmail.com>
2023-08-28 23:37:30 -0300

    Concluido

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# projeto1
+# Extensões Instaladas
+
+* C/C++ IntelliSense, debugging, and code browsing / Extension Pack / Themes / CMake / CMake Tools;
+=> Pacote necessário da Microsoft para rodar o C++;
+
+* Português (Brazil) Language Pack for Visual Studio Code;
+=> Pacote para configuração da Língua Portuguesa do Brasileira para o VS-Code;
+
+*C/C++ Compile Run;
+=> Compilador do C/C++;
+
+*Drácula Oficial;
+=> Tema para melhorar o visual enquando realizamos os códigos no VS-Code.
+
+# Definir O Tema Drácula
+
+* Definindo um Tema: Após a instalação ou escolhendo a extensão que já se encontra instalado, clica no botão DEFINIR TEMA DE COR e seleciona Drácula ou Drácula Soft.
--- meuprog3.cpp
+++ meuprog3.cpp
@@ -7,22 +7,23 @@ int main(void) {
     using namespace std;
 
 
-    double A = 5.0;
-    double B = 3.0;
+    float A = 5.0;
+    float B = 3.0;
 
-    double soma = A + B;
-    double subtracao = A - B;
-    double multiplicacao = A * B;
-    double divisao = A / B;
-    double resto = fmod(A, B);
-
-    cout << "Soma = " << fixed << setprecision(2) << soma << endl;
-    cout << "Subtração = " << fixed << setprecision(2) << subtracao << endl;
-    cout << "Multiplicação = " << fixed << setprecision(2) << multiplicacao << endl;
-    cout << "Divisão = " << fixed << setprecision(2) << divisao << endl;
-    cout << "Resto da Divisão = " << fixed << setprecision(2) << resto << endl;
+    float soma = A + B;
+    float subtracao = A - B;
+    float multiplicacao = A * B;
+    float divisao = (A / B);
     
-   
+    
+
+    
+    cout << "Soma = " << fixed << setprecision(1) << soma << endl;
+    cout << "Subtração = " << fixed << setprecision(1) << subtracao << endl;
+    cout << "Multiplicação = " << fixed << setprecision(1) << multiplicacao << endl;
+    cout << "Divisão = " << fixed << setprecision(1) << divisao << endl;
+    
+     
 
     return 0;
 }
