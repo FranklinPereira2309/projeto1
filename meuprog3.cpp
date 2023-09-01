55d55e1a402d613936f207c11eba4b454c7897a8
Franklin Menezes <fmpereira2309@hotmail.com>
2023-09-01 09:42:46 -0300

    Ajuste nas casas decimais

--- meuprog3.cpp
+++ meuprog3.cpp
@@ -7,13 +7,15 @@ int main(void) {
     using namespace std;
 
 
-    float A = 5.0;
-    float B = 3.0;
+    float A = 5.0, B = 3.0, corrigirCasasDecimais;
+    
 
     float soma = A + B;
     float subtracao = A - B;
     float multiplicacao = A * B;
     float divisao = (A / B);
+
+    corrigirCasasDecimais = floorf(divisao * 10) / 10;
     
     
 
@@ -21,7 +23,7 @@ int main(void) {
     cout << "Soma = " << fixed << setprecision(1) << soma << endl;
     cout << "Subtração = " << fixed << setprecision(1) << subtracao << endl;
     cout << "Multiplicação = " << fixed << setprecision(1) << multiplicacao << endl;
-    cout << "Divisão = " << fixed << setprecision(1) << divisao << endl;
+    cout << "Divisão = " << fixed << setprecision(1) << corrigirCasasDecimais << endl;
     
      
 
